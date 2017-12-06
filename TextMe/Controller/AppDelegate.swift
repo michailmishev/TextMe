a0e848f696392f0185ec56f2266ebb591698b9c1
Michail Mishev <m.mishev1@gmail.com>
2017-12-06 15:28:40 +0200

    Initialize Firebase

--- TextMe/Controller/AppDelegate.swift
+++ TextMe/Controller/AppDelegate.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import Firebase
+
+
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,7 +18,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        //initialise firebase:
+        FirebaseApp.configure()
+        
+        let myDatabase = Database.database().reference()
+        myDatabase.setValue("We've got data!")
+        
         return true
     }
 
