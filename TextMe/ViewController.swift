09a1d65e9ab27a4766fed74d5ce31b66606c5535
Michail Mishev <m.mishev1@gmail.com>
2017-12-06 14:21:58 +0200

    Initial Commit

---
+++ TextMe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TextMe
+//
+//  Created by Michail Mishev on 6/12/17.
+//  Copyright © 2017 Michail Mishev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
